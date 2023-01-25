7636dab2e28f299416259e9ff1ed26540d321e1d
ocricci <oc.ricci@gmail.com>
2023-01-25 14:08:39 -0300

    feat(learncpp.com): multiply

--- multiply.cpp
+++ multiply.cpp
@@ -5,5 +5,9 @@ int main() {
     int x{};
     std::cin >> x;
     std::cout << "You entered: " << x << " ( x2 ): " << x * 2 << '\n';
+
+    // quest
+    std::cout << "Double is: " << x * 2 << "\nTriple is: " << x * 3 << std::endl;
+
     return 0;
 }
