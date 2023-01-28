4b8c65865911fe48f55c36f93f32ef6381f5e687
ocricci <oc.ricci@gmail.com>
2023-01-28 10:16:39 -0300

    feat(learncpp.com): sizeof

--- chapter_four/main.cpp
+++ chapter_four/main.cpp
@@ -4,7 +4,17 @@ void voidz() {
     std::cout << "Voidz" << std::endl;
 }
 
+void sizeofz() {
+    std::cout << "Sizeof\n";
+    std::cout << "short: " << sizeof(short) << "\n";
+    std::cout << "int: " << sizeof(int) << "\n";
+    std::cout << "float: " << sizeof(float) << "\n";
+    std::cout << "bool: " << sizeof(bool) << "\n";
+    std::cout << "long long: " << sizeof(long long) << "\n";
+}
+
 int main() {
     voidz();
+    sizeofz();
 }
 
