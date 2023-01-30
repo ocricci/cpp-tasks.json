4256f1a1e5a9213e85f9690ab7b82b16d90beef7
ocricci <oc.ricci@gmail.com>
2023-01-30 20:03:29 -0300

    feat(learncpp.com): bitwise

--- chapter_O/main.cpp
+++ chapter_O/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <bitset>
+#include <cstdint>
 
 void rotateDaBitz(std::bitset<4> bits) {
     std::cout << "---------------------" << '\n';
@@ -21,9 +22,109 @@ void superRotateDaBitz(std::bitset<4> bits) {
     std::cout << "Rotated bits: " << std::bitset<4>{(bits<<1) | (bits>>3)} << '\n';
 }
 
+void bitMasks() {
+    [[maybe_unused]] constexpr std::uint8_t mask0{0b0000'0001};
+    [[maybe_unused]] constexpr std::uint8_t mask1{0b0000'0010};
+    [[maybe_unused]] constexpr std::uint8_t mask2{0b0000'0100};
+    [[maybe_unused]] constexpr std::uint8_t mask3{0b0000'1000};
+    [[maybe_unused]] constexpr std::uint8_t mask4{0b0001'0000};
+    [[maybe_unused]] constexpr std::uint8_t mask5{0b0010'0000};
+    [[maybe_unused]] constexpr std::uint8_t mask6{0b0100'0000};
+    [[maybe_unused]] constexpr std::uint8_t mask7{0b1000'0000};
+
+    std::uint8_t flags{0b0000'0101};
+    std::cout << (flags & mask0 ? "on" : "off") << '\n';
+    std::cout << (flags & mask1 ? "on" : "off") << '\n';
+    std::cout << (flags & mask2 ? "on" : "off") << '\n';
+
+    flags |= mask1;
+    //std::cout << (flags & mask1 ? "on" : "off") << '\n';
+
+    //flags |= (mask4 | mask5);
+    
+    //std::cout << flags << '\n';
+    //std::uint8_t x{0b0000'0101};
+    //x &= ~mask0;
+    //std::cout << (x & mask0 ? "xon" : "xoff") << '\n';
+    flags ^= mask2;
+    /*
+    constexpr std::uint8_t mask0{ 0x01 }; // hex for 0000 0001
+    constexpr std::uint8_t mask1{ 0x02 }; // hex for 0000 0010
+    constexpr std::uint8_t mask2{ 0x04 }; // hex for 0000 0100
+    constexpr std::uint8_t mask3{ 0x08 }; // hex for 0000 1000
+    constexpr std::uint8_t mask4{ 0x10 }; // hex for 0001 0000
+    constexpr std::uint8_t mask5{ 0x20 }; // hex for 0010 0000
+    constexpr std::uint8_t mask6{ 0x40 }; // hex for 0100 0000
+    constexpr std::uint8_t mask7{ 0x80 }; // hex for 1000 0000 */
+}
+
+void gameEx() {
+    // Define a bunch of physical/emotional states
+    /*
+	[[maybe_unused]] constexpr std::int8_t isHungry{	1 << 0 }; // 0000 0001
+	[[maybe_unused]] constexpr std::int8_t isSad{		1 << 1 }; // 0000 0010
+	[[maybe_unused]] constexpr std::int8_t isMad{		1 << 2 }; // 0000 0100
+	[[maybe_unused]] constexpr std::int8_t isHappy{	1 << 3 }; // 0000 1000
+	[[maybe_unused]] constexpr std::int8_t isLaughing{ 	1 << 4 }; // 0001 0000
+	[[maybe_unused]] constexpr std::int8_t isAsleep{	1 << 5 }; // 0010 0000
+	[[maybe_unused]] constexpr std::int8_t isDead{		1 << 6 }; // 0100 0000
+	[[maybe_unused]] constexpr std::int8_t isCrying{	1 << 7 }; // 1000 0000
+
+	std::int8_t me{}; // all flags/options turned off to start
+	me |= (isHappy | isLaughing); // I am happy and laughing
+	me &= ~isLaughing; // I am no longer laughing
+
+	// Query a few states
+	// (we'll use static_cast<bool> to interpret the results as a boolean value)
+	std::cout << "I am happy? " << static_cast<bool>(me & isHappy) << '\n';
+	std::cout << "I am laughing? " << static_cast<bool>(me & isLaughing) << '\n'; */
+            // Define a bunch of physical/emotional states
+	[[maybe_unused]] constexpr std::bitset<8> isHungry{	0b0000'0001 };
+	[[maybe_unused]] constexpr std::bitset<8> isSad{	0b0000'0010 };
+	[[maybe_unused]] constexpr std::bitset<8> isMad{	0b0000'0100 };
+	[[maybe_unused]] constexpr std::bitset<8> isHappy{	0b0000'1000 };
+	[[maybe_unused]] constexpr std::bitset<8> isLaughing{	0b0001'0000 };
+	[[maybe_unused]] constexpr std::bitset<8> isAsleep{	0b0010'0000 };
+	[[maybe_unused]] constexpr std::bitset<8> isDead{	0b0100'0000 };
+	[[maybe_unused]] constexpr std::bitset<8> isCrying{	0b1000'0000 };
+
+
+	std::bitset<8> me{}; // all flags/options turned off to start
+    std::cout << "size of bitset<8> is: " << sizeof(me) << '\n';
+
+	me |= (isHappy | isLaughing); // I am happy and laughing
+	me &= ~isLaughing; // I am no longer laughing
+
+	// Query a few states (we use the any() function to see if any bits remain set)
+	std::cout << "I am happy? " << (me & isHappy).any() << '\n';
+	std::cout << "I am laughing? " << (me & isLaughing).any() << '\n';
+}
+
+void quiz() {
+    [[maybe_unused]] constexpr std::uint8_t option_viewed{ 0x01 };
+    [[maybe_unused]] constexpr std::uint8_t option_edited{ 0x02 };
+    [[maybe_unused]] constexpr std::uint8_t option_favorited{ 0x04 };
+    [[maybe_unused]] constexpr std::uint8_t option_shared{ 0x08 };
+    [[maybe_unused]] constexpr std::uint8_t option_deleted{ 0x10 };
+
+    std::uint8_t myArticleFlags{ option_favorited };
+
+    // set article as viewed
+    myArticleFlags |= option_viewed;
+
+    // check if article is deleted
+    (myArticleFlags & option_deleted) ? std::cout << "deleted" : std::cout << "not deleted" << '\n';
+
+    // clear favorite flag
+    //myArcticleFlags &= ~option_favorited;
+
+    std::cout << std::bitset<8>{ myArticleFlags } << '\n';
+}
+
+
 int main() {
-    std::bitset<8> bits{ 0b0000'0001 };
-    bits.set(3);
+   std::bitset<8> bits{ 0b0000'0001 };
+   bits.set(3);
     bits.flip(4);
     bits.reset(4);
 
@@ -54,6 +155,8 @@ int main() {
     superRotateDaBitz(std::bitset<4>{0b1100});
     rotateDaBitz(std::bitset<4>{0b0101});
     rotateDaBitz(std::bitset<4>{0b0001});
-
+    bitMasks();
+    gameEx();
+    quiz();
     return 0;
 }
