dc4332311550bdedce69bc18ce0934f2e61ba3fc
ocricci <oc.ricci@gmail.com>
2023-02-01 19:53:08 -0300

    feat(learncpp.com): if and switch

--- chapter_seven/switchz/add.cpp
+++ chapter_seven/switchz/add.cpp
@@ -3,21 +3,23 @@
 void printDigitName(int digit) {
     switch (digit) {
         case 1:
-            std::cout << "One" << std::endl;
-        return;
+            std::cout << "One" << '\n';
+        break;
 
         case 2:
-            std::cout << "Two" << std::endl;
-        return;
+            std::cout << "Two" << '\n';
+        break;
 
         case 3:
-            std::cout << "Three" << std::endl;
-        return;
+            std::cout << "Three" << '\n';
+        break;
 
         default:
-            std::cout << "Unknown" << std::endl;
-        return;
+            std::cout << "Unknown" << '\n';
+        break;
     }
+
+    std::cout << "function continues because swtich used break instead of return" << std::endl;
 }
 
 int main() {
@@ -25,7 +27,7 @@ int main() {
     int digit{0};
     std::cin >> digit;
     if (digit < 1 || digit > 3) {
-        std::cout << digit << " is not between 1 and 3... " << "Never ever do it again !" << std::endl;
+        std::cout << digit << " is not between 1 and 3... " << "Never ever do it again !" << '\n';
         return 0;
     }
 
