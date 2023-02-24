4c85179c6efb91476bb69f14ce97001c961ae00c
ocricci <oc.ricci@gmail.com>
2023-02-24 20:00:39 -0300

    feat(learncpp.com): goto end

--- chapter_seven/gotoz/main.cpp
+++ chapter_seven/gotoz/main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+void printCats(bool printCat) {
+    if (false == printCat) {
+        goto end;
+    }
+
+    std::cout << "Top cat" << "\n";
+
+end:
+    std::cout << "End of printing cats" << std::endl;
+}
+
 int main() {
 enterANumber:
     std::cout << "Enter a number greater than 50: " << '\n';
@@ -10,5 +21,7 @@ enterANumber:
         goto enterANumber;
     }
     std::cout << "Continues.. " << '\n';
+    printCats(true);
+    printCats(false);
     return 0;
 }
